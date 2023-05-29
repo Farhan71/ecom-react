2b7971669915bb46a9e4a7fceb85de5b6d4818b2
Farhan71 <farhanitnstu@gmail.com>
2023-05-29 21:49:20 +0600

    home and collection done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Announcement from '../components/Announcement'
+import { Slider } from '../components/Slider'
+import Categories from '../components/Categories'
 
 const Home = () => {
   return (
     <div>
+        <Announcement></Announcement>
         <Navbar></Navbar> 
+        <Slider></Slider>
+        <Categories></Categories>
     </div>
   )
 }
